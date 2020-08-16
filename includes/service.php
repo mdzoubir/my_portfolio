6711cce46215e98135a8b2333ad802017b815f63
mdzoubir <57962405+mdzoubir@users.noreply.github.com>
2020-08-16 01:03:57 +0100

    page hero done

---
+++ includes/service.php
@@ -0,0 +1,10 @@
+<section id="services">
+    <div class="service container"> 
+        <div class="service-top">
+            <h1 class="section-title">Services</h1>
+        </div>
+        <div class="service-bottom">
+
+        </div>
+    </div>
+</section>
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php include('includes/header.php'); ?>
+<!-- hero section -->
+        <section id="hero">
+            <div class="hero container">
+                <div>
+                    <h1>Hello, <span></span></h1>
+                    <h1>My Name is <span></span></h1>
+                    <h1>Mohamed <span></span></h1>
+                    <a href="#" type="button" class="cta">Portfolio</a>
+                </div>
+            </div>
+        </section>
+
+<!-- End Hero section -->
+<?php   
+// service section
+include('includes/service.php');
+// end service section
+
+
+// footer 
+ include('includes/footer.php'); 
+//end footer
+?>
+
+
+
+
+
+
+
+
+<?php include('includes/footer.php'); ?>
+    
