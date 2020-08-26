aa01afa49692b5c0155258f5549f319d6c31e50a
mdzoubir <57962405+mdzoubir@users.noreply.github.com>
2020-08-26 12:50:44 +0100

    add navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -13,7 +13,7 @@
             <li><a href="#projects" data-after="Projects">Projects</a></li>
             <li><a href="#about" data-after="About">About</a></li>
             <li><a href="#contact" data-after="Contact">Contact</a></li>
-            <li><a href="#ogin.php" data-after="Contact">login</a></li>
+            <li><a href="#ogin.php" data-after="Login">login</a></li>
           </ul>
         </div>
       </div>
